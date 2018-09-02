9d593bdf55bae483b5349f97ddeceb3222df98b4
Kishan Parikh <kjp2376@vt.edu>
2018-09-02 16:15:21 -0400

    Reformatted file and added square root functionality

--- environment.cpp
+++ environment.cpp
@@ -11,7 +11,7 @@ Helper Functions
 **********************************************************************/
 
 // predicate, the number of args is nargs
-bool nargs_equal(const std::vector<Expression> & args, unsigned nargs){
+bool nargs_equal(const std::vector<Expression> &args, unsigned nargs) {
   return args.size() == nargs;
 }
 
@@ -21,20 +21,19 @@ typedef'd Procedure function pointer.
 **********************************************************************/
 
 // the default procedure always returns an expresison of type None
-Expression default_proc(const std::vector<Expression> & args){
+Expression default_proc(const std::vector<Expression> &args) {
   args.size(); // make compiler happy we used this parameter
   return Expression();
 };
 
-Expression add(const std::vector<Expression> & args){
+Expression add(const std::vector<Expression> &args) {
 
-  // check all aruments are numbers, while adding
+  // check all arguments are numbers, while adding
   double result = 0;
-  for( auto & a :args){
-    if(a.isHeadNumber()){
-      result += a.head().asNumber();      
-    }
-    else{
+  for (auto &a :args) {
+    if (a.isHeadNumber()) {
+      result += a.head().asNumber();
+    } else {
       throw SemanticError("Error in call to add, argument not a number");
     }
   }
@@ -42,15 +41,14 @@ Expression add(const std::vector<Expression> & args){
   return Expression(result);
 };
 
-Expression mul(const std::vector<Expression> & args){
- 
+Expression mul(const std::vector<Expression> &args) {
+
   // check all aruments are numbers, while multiplying
   double result = 1;
-  for( auto & a :args){
-    if(a.isHeadNumber()){
-      result *= a.head().asNumber();      
-    }
-    else{
+  for (auto &a :args) {
+    if (a.isHeadNumber()) {
+      result *= a.head().asNumber();
+    } else {
       throw SemanticError("Error in call to mul, argument not a number");
     }
   }
@@ -58,80 +56,90 @@ Expression mul(const std::vector<Expression> & args){
   return Expression(result);
 };
 
-Expression subneg(const std::vector<Expression> & args){
+Expression subneg(const std::vector<Expression> &args) {
 
   double result = 0;
 
   // preconditions
-  if(nargs_equal(args,1)){
-    if(args[0].isHeadNumber()){
+  if (nargs_equal(args, 1)) {
+    if (args[0].isHeadNumber()) {
       result = -args[0].head().asNumber();
-    }
-    else{
+    } else {
       throw SemanticError("Error in call to negate: invalid argument.");
     }
-  }
-  else if(nargs_equal(args,2)){
-    if( (args[0].isHeadNumber()) && (args[1].isHeadNumber()) ){
+  } else if (nargs_equal(args, 2)) {
+    if ((args[0].isHeadNumber()) && (args[1].isHeadNumber())) {
       result = args[0].head().asNumber() - args[1].head().asNumber();
-    }
-    else{      
+    } else {
       throw SemanticError("Error in call to subtraction: invalid argument.");
     }
-  }
-  else{
+  } else {
     throw SemanticError("Error in call to subtraction or negation: invalid number of arguments.");
   }
 
   return Expression(result);
 };
 
-Expression div(const std::vector<Expression> & args){
+Expression div(const std::vector<Expression> &args) {
 
-  double result = 0;  
+  double result = 0;
 
-  if(nargs_equal(args,2)){
-    if( (args[0].isHeadNumber()) && (args[1].isHeadNumber()) ){
+  if (nargs_equal(args, 2)) {
+    if ((args[0].isHeadNumber()) && (args[1].isHeadNumber())) {
       result = args[0].head().asNumber() / args[1].head().asNumber();
-    }
-    else{      
+    } else {
       throw SemanticError("Error in call to division: invalid argument.");
     }
-  }
-  else{
+  } else {
     throw SemanticError("Error in call to division: invalid number of arguments.");
   }
   return Expression(result);
 };
 
+Expression sqrt(const std::vector<Expression> &args) {
+
+  double result = 0;
+  // check if one argument
+  if (args.size() != 1) {
+    throw SemanticError("Error: invalid number of arguments for sqrt");
+  } else if (args.at(0).isHeadNumber() && args.at(0).head().asNumber() > 0) {
+    result = std::sqrt(args.at(0).head().asNumber());
+  } else {
+    throw SemanticError("Error in call to sqrt, argument not a number");
+  }
+  return Expression(result);
+}
+
 const double PI = std::atan2(0, -1);
 const double EXP = std::exp(1);
 
-Environment::Environment(){
+Environment::Environment() {
 
   reset();
 }
 
-bool Environment::is_known(const Atom & sym) const{
-  if(!sym.isSymbol()) return false;
-  
+bool Environment::is_known(const Atom &sym) const {
+  if (!sym.isSymbol())
+    return false;
+
   return envmap.find(sym.asSymbol()) != envmap.end();
 }
 
-bool Environment::is_exp(const Atom & sym) const{
-  if(!sym.isSymbol()) return false;
-  
+bool Environment::is_exp(const Atom &sym) const {
+  if (!sym.isSymbol())
+    return false;
+
   auto result = envmap.find(sym.asSymbol());
   return (result != envmap.end()) && (result->second.type == ExpressionType);
 }
 
-Expression Environment::get_exp(const Atom & sym) const{
+Expression Environment::get_exp(const Atom &sym) const {
 
   Expression exp;
-  
-  if(sym.isSymbol()){
+
+  if (sym.isSymbol()) {
     auto result = envmap.find(sym.asSymbol());
-    if((result != envmap.end()) && (result->second.type == ExpressionType)){
+    if ((result != envmap.end()) && (result->second.type == ExpressionType)) {
       exp = result->second.exp;
     }
   }
@@ -139,34 +147,35 @@ Expression Environment::get_exp(const Atom & sym) const{
   return exp;
 }
 
-void Environment::add_exp(const Atom & sym, const Expression & exp){
+void Environment::add_exp(const Atom &sym, const Expression &exp) {
 
-  if(!sym.isSymbol()){
+  if (!sym.isSymbol()) {
     throw SemanticError("Attempt to add non-symbol to environment");
   }
-    
+
   // error if overwriting symbol map
-  if(envmap.find(sym.asSymbol()) != envmap.end()){
+  if (envmap.find(sym.asSymbol()) != envmap.end()) {
     throw SemanticError("Attempt to overwrite symbol in environemnt");
   }
 
-  envmap.emplace(sym.asSymbol(), EnvResult(ExpressionType, exp)); 
+  envmap.emplace(sym.asSymbol(), EnvResult(ExpressionType, exp));
 }
 
-bool Environment::is_proc(const Atom & sym) const{
-  if(!sym.isSymbol()) return false;
-  
+bool Environment::is_proc(const Atom &sym) const {
+  if (!sym.isSymbol())
+    return false;
+
   auto result = envmap.find(sym.asSymbol());
   return (result != envmap.end()) && (result->second.type == ProcedureType);
 }
 
-Procedure Environment::get_proc(const Atom & sym) const{
+Procedure Environment::get_proc(const Atom &sym) const {
 
   //Procedure proc = default_proc;
 
-  if(sym.isSymbol()){
+  if (sym.isSymbol()) {
     auto result = envmap.find(sym.asSymbol());
-    if((result != envmap.end()) && (result->second.type == ProcedureType)){
+    if ((result != envmap.end()) && (result->second.type == ProcedureType)) {
       return result->second.proc;
     }
   }
@@ -178,10 +187,10 @@ Procedure Environment::get_proc(const Atom & sym) const{
 Reset the environment to the default state. First remove all entries and
 then re-add the default ones.
  */
-void Environment::reset(){
+void Environment::reset() {
 
   envmap.clear();
-  
+
   // Built-In value of pi
   envmap.emplace("pi", EnvResult(ExpressionType, Expression(PI)));
 
@@ -189,14 +198,17 @@ void Environment::reset(){
   envmap.emplace("e", EnvResult(ExpressionType, Expression(EXP)));
 
   // Procedure: add;
-  envmap.emplace("+", EnvResult(ProcedureType, add)); 
+  envmap.emplace("+", EnvResult(ProcedureType, add));
 
   // Procedure: subneg;
-  envmap.emplace("-", EnvResult(ProcedureType, subneg)); 
+  envmap.emplace("-", EnvResult(ProcedureType, subneg));
 
   // Procedure: mul;
-  envmap.emplace("*", EnvResult(ProcedureType, mul)); 
+  envmap.emplace("*", EnvResult(ProcedureType, mul));
 
   // Procedure: div;
-  envmap.emplace("/", EnvResult(ProcedureType, div)); 
+  envmap.emplace("/", EnvResult(ProcedureType, div));
+
+  // Procedure: sqrt;
+  envmap.emplace("sqrt", EnvResult(ProcedureType, sqrt));
 }
--- interpreter_tests.cpp
+++ interpreter_tests.cpp
@@ -9,15 +9,15 @@
 #include "interpreter.hpp"
 #include "expression.hpp"
 
-Expression run(const std::string & program){
-  
+Expression run(const std::string &program) {
+
   std::istringstream iss(program);
-    
+
   Interpreter interp;
-    
+
   bool ok = interp.parseStream(iss);
-  if(!ok){
-    std::cerr << "Failed to parse: " << program << std::endl; 
+  if (!ok) {
+    std::cerr << "Failed to parse: " << program << std::endl;
   }
   REQUIRE(ok == true);
 
@@ -27,12 +27,12 @@ Expression run(const std::string & program){
   return result;
 }
 
-TEST_CASE( "Test Interpreter parser with expected input", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with expected input", "[interpreter]") {
 
   std::string program = "(begin (define r 10) (* pi (* r r)))";
 
   std::istringstream iss(program);
- 
+
   Interpreter interp;
 
   bool ok = interp.parseStream(iss);
@@ -40,13 +40,13 @@ TEST_CASE( "Test Interpreter parser with expected input", "[interpreter]" ) {
   REQUIRE(ok == true);
 }
 
-TEST_CASE( "Test Interpreter parser with numerical literals", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with numerical literals", "[interpreter]") {
 
   std::vector<std::string> programs = {"(1)", "(+1)", "(+1e+0)", "(1e-0)"};
-  
-  for(auto program : programs){
+
+  for (auto program : programs) {
     std::istringstream iss(program);
- 
+
     Interpreter interp;
 
     bool ok = interp.parseStream(iss);
@@ -56,7 +56,7 @@ TEST_CASE( "Test Interpreter parser with numerical literals", "[interpreter]" ) 
 
   {
     std::istringstream iss("(define x 1abc)");
-    
+
     Interpreter interp;
 
     bool ok = interp.parseStream(iss);
@@ -65,17 +65,17 @@ TEST_CASE( "Test Interpreter parser with numerical literals", "[interpreter]" ) 
   }
 }
 
-TEST_CASE( "Test Interpreter parser with truncated input", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with truncated input", "[interpreter]") {
 
   {
     std::string program = "(f";
     std::istringstream iss(program);
-  
+
     Interpreter interp;
     bool ok = interp.parseStream(iss);
     REQUIRE(ok == false);
   }
-  
+
   {
     std::string program = "(begin (define r 10) (* pi (* r r";
     std::istringstream iss(program);
@@ -86,7 +86,7 @@ TEST_CASE( "Test Interpreter parser with truncated input", "[interpreter]" ) {
   }
 }
 
-TEST_CASE( "Test Interpreter parser with extra input", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with extra input", "[interpreter]") {
 
   std::string program = "(begin (define r 10) (* pi (* r r))) )";
   std::istringstream iss(program);
@@ -98,11 +98,11 @@ TEST_CASE( "Test Interpreter parser with extra input", "[interpreter]" ) {
   REQUIRE(ok == false);
 }
 
-TEST_CASE( "Test Interpreter parser with single non-keyword", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with single non-keyword", "[interpreter]") {
 
   std::string program = "hello";
   std::istringstream iss(program);
-  
+
   Interpreter interp;
 
   bool ok = interp.parseStream(iss);
@@ -110,11 +110,11 @@ TEST_CASE( "Test Interpreter parser with single non-keyword", "[interpreter]" ) 
   REQUIRE(ok == false);
 }
 
-TEST_CASE( "Test Interpreter parser with empty input", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with empty input", "[interpreter]") {
 
   std::string program;
   std::istringstream iss(program);
-  
+
   Interpreter interp;
 
   bool ok = interp.parseStream(iss);
@@ -122,11 +122,11 @@ TEST_CASE( "Test Interpreter parser with empty input", "[interpreter]" ) {
   REQUIRE(ok == false);
 }
 
-TEST_CASE( "Test Interpreter parser with empty expression", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with empty expression", "[interpreter]") {
 
   std::string program = "( )";
   std::istringstream iss(program);
-  
+
   Interpreter interp;
 
   bool ok = interp.parseStream(iss);
@@ -134,11 +134,11 @@ TEST_CASE( "Test Interpreter parser with empty expression", "[interpreter]" ) {
   REQUIRE(ok == false);
 }
 
-TEST_CASE( "Test Interpreter parser with bad number string", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with bad number string", "[interpreter]") {
 
   std::string program = "(1abc)";
   std::istringstream iss(program);
-  
+
   Interpreter interp;
 
   bool ok = interp.parseStream(iss);
@@ -146,11 +146,11 @@ TEST_CASE( "Test Interpreter parser with bad number string", "[interpreter]" ) {
   REQUIRE(ok == false);
 }
 
-TEST_CASE( "Test Interpreter parser with incorrect input. Regression Test", "[interpreter]" ) {
+TEST_CASE("Test Interpreter parser with incorrect input. Regression Test", "[interpreter]") {
 
   std::string program = "(+ 1 2) (+ 3 4)";
   std::istringstream iss(program);
-  
+
   Interpreter interp;
 
   bool ok = interp.parseStream(iss);
@@ -158,8 +158,8 @@ TEST_CASE( "Test Interpreter parser with incorrect input. Regression Test", "[in
   REQUIRE(ok == false);
 }
 
-TEST_CASE( "Test Interpreter result with literal expressions", "[interpreter]" ) {
-  
+TEST_CASE("Test Interpreter result with literal expressions", "[interpreter]") {
+
   { // Number
     std::string program = "(4)";
     Expression result = run(program);
@@ -174,7 +174,7 @@ TEST_CASE( "Test Interpreter result with literal expressions", "[interpreter]" )
 
 }
 
-TEST_CASE( "Test Interpreter result with simple procedures (add)", "[interpreter]" ) {
+TEST_CASE("Test Interpreter result with simple procedures (add)", "[interpreter]") {
 
   { // add, binary case
     std::string program = "(+ 1 2)";
@@ -182,7 +182,7 @@ TEST_CASE( "Test Interpreter result with simple procedures (add)", "[interpreter
     Expression result = run(program);
     REQUIRE(result == Expression(3.));
   }
-  
+
   { // add, 3-ary case
     std::string program = "(+ 1 2 3)";
     INFO(program);
@@ -197,8 +197,18 @@ TEST_CASE( "Test Interpreter result with simple procedures (add)", "[interpreter
     REQUIRE(result == Expression(21.));
   }
 }
-  
-TEST_CASE( "Test Interpreter special forms: begin and define", "[interpreter]" ) {
+
+TEST_CASE("Test Interpreter result with simple procedures (sqrt)", "[interpreter]") {
+
+  { // sqrt, binary case
+    std::string program = "(sqrt 4)";
+    INFO(program);
+    Expression result = run(program);
+    REQUIRE(result == Expression(2.));
+  }
+}
+
+TEST_CASE("Test Interpreter special forms: begin and define", "[interpreter]") {
 
   {
     std::string program = "(define answer 42)";
@@ -213,7 +223,7 @@ TEST_CASE( "Test Interpreter special forms: begin and define", "[interpreter]" )
     Expression result = run(program);
     REQUIRE(result == Expression(42.));
   }
-  
+
   {
     std::string program = "(begin (define answer (+ 9 11)) (answer))";
     INFO(program);
@@ -229,7 +239,7 @@ TEST_CASE( "Test Interpreter special forms: begin and define", "[interpreter]" )
   }
 }
 
-TEST_CASE( "Test a medium-sized expression", "[interpreter]" ) {
+TEST_CASE("Test a medium-sized expression", "[interpreter]") {
 
   {
     std::string program = "(+ (+ 10 1) (+ 30 (+ 1 1)))";
@@ -238,87 +248,101 @@ TEST_CASE( "Test a medium-sized expression", "[interpreter]" ) {
   }
 }
 
-TEST_CASE( "Test arithmetic procedures", "[interpreter]" ) {
+TEST_CASE("Test arithmetic procedures", "[interpreter]") {
 
   {
     std::vector<std::string> programs = {"(+ 1 -2)",
-					 "(+ -3 1 1)",
-					 "(- 1)",
-					 "(- 1 2)",
-					 "(* 1 -1)",
-					 "(* 1 1 -1)",
-					 "(/ -1 1)",
-					 "(/ 1 -1)"};
+                                         "(+ -3 1 1)",
+                                         "(- 1)",
+                                         "(- 1 2)",
+                                         "(* 1 -1)",
+                                         "(* 1 1 -1)",
+                                         "(/ -1 1)",
+                                         "(/ 1 -1)"};
 
-    for(auto s : programs){
+    for (auto s : programs) {
       Expression result = run(s);
       REQUIRE(result == Expression(-1.));
     }
   }
 }
 
+TEST_CASE("Test some semantically invalid expresions", "[interpreter]") {
 
-TEST_CASE( "Test some semantically invalid expresions", "[interpreter]" ) {
-  
   std::vector<std::string> programs = {"(@ none)", // so such procedure
-				       "(- 1 1 2)", // too many arguments
-				       "(define begin 1)", // redefine special form
-				       "(define pi 3.14)"}; // redefine builtin symbol
-    for(auto s : programs){
-      Interpreter interp;
+                                       "(- 1 1 2)", // too many arguments
+                                       "(define begin 1)", // redefine special form
+                                       "(define pi 3.14)"}; // redefine builtin symbol
+  for (auto s : programs) {
+    Interpreter interp;
 
-      std::istringstream iss(s);
-      
-      bool ok = interp.parseStream(iss);
-      REQUIRE(ok == true);
-      
-      REQUIRE_THROWS_AS(interp.evaluate(), SemanticError);
-    }
+    std::istringstream iss(s);
+
+    bool ok = interp.parseStream(iss);
+    REQUIRE(ok == true);
+
+    REQUIRE_THROWS_AS(interp.evaluate(), SemanticError);
+  }
 }
 
-TEST_CASE( "Test for exceptions from semantically incorrect input", "[interpreter]" ) {
+TEST_CASE("Test for exceptions from semantically incorrect input", "[interpreter]") {
 
   std::string input = R"(
 (+ 1 a)
 )";
 
   Interpreter interp;
-  
+
   std::istringstream iss(input);
-  
+
   bool ok = interp.parseStream(iss);
   REQUIRE(ok == true);
-  
+
   REQUIRE_THROWS_AS(interp.evaluate(), SemanticError);
 }
 
-TEST_CASE( "Test malformed define", "[interpreter]" ) {
+TEST_CASE("Test malformed define", "[interpreter]") {
 
-    std::string input = R"(
+  std::string input = R"(
 (define a 1 2)
 )";
 
   Interpreter interp;
-  
+
   std::istringstream iss(input);
-  
+
   bool ok = interp.parseStream(iss);
   REQUIRE(ok == true);
-  
+
   REQUIRE_THROWS_AS(interp.evaluate(), SemanticError);
 }
 
-TEST_CASE( "Test using number as procedure", "[interpreter]" ) {
-    std::string input = R"(
+TEST_CASE("Test using number as procedure", "[interpreter]") {
+  std::string input = R"(
 (1 2 3)
 )";
 
   Interpreter interp;
-  
+
   std::istringstream iss(input);
-  
+
   bool ok = interp.parseStream(iss);
   REQUIRE(ok == true);
-  
+
+  REQUIRE_THROWS_AS(interp.evaluate(), SemanticError);
+}
+
+TEST_CASE("Test invalid sqrt procedure", "[interpreter]") {
+  std::string input = R"(
+(sqrt -4)
+)";
+
+  Interpreter interp;
+
+  std::istringstream iss(input);
+
+  bool ok = interp.parseStream(iss);
+  REQUIRE(ok == true);
+
   REQUIRE_THROWS_AS(interp.evaluate(), SemanticError);
 }
